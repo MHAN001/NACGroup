37644f237edc59788a003df9bceb40c37afa5655
Deren <deren@DerendeMBP.fios-router.home>
2020-07-01 00:46:40 -0400

    add web service

---
+++ nacg-web/src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="App-link">
+          Go NACG!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ nacg-web/src/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+const Routes = () => (
+    <main>
+        <Switch>
+            <Route path="/" component={App} /> >
+        </Switch>
+    </main>
+);
+
+export const MyApp =() => {
+    return (
+        <Router>
+            <div>
+                <Routes />
+            </div>
+        </Router>
+    )
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MyApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
