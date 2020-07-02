cc338a1fddfcc9c6423b7d9923e5ae75a1b7e108
Deren <deren@DerendeMBP.fios-router.home>
2020-07-02 00:39:32 -0400

    hookup ui & backend

--- nacg-web/src/components/App.tsx
+++ nacg-web/src/components/App.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <a className="App-link">
-          Go NACG!
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+    state = {
+        str: ""
+    }
 
-export default App;
+    componentDidMount () {
+        fetch('http://localhost:8080/hello/world')
+            .then(response => response.text())
+            .then(res =>{
+                this.setState({str: res});
+            });
+    }
+
+    render() {
+        return (
+        <div className="App">
+            <header className="App-header">
+                <a className="App-link">
+                    {this.state.str}
+                </a>
+            </header>
+        </div>
+        );
+    }
+};
